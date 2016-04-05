9cd7bee49756ae8db88e9aea96bddfd8bcba7463
DavidMonts <montecillodavid.spingine@gmail.com>
2016-04-05 18:29:02 +0800

    initial

---
+++ src/components/ContentPage/ContentPage.js
@@ -0,0 +1,44 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React, { Component, PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './ContentPage.scss';
+//
+//var MapboxClient = require('mapbox');
+//var client = new MapboxClient('pk.eyJ1IjoianJvam9vIiwiYSI6IjU5YWMxNTI2MzA2MzlmNWUxNWNhOGE2M2QwZTI2YTExIn0.cNrKr17Aley6AYfK245vlA');
+
+
+class ContentPage extends Component {
+
+  static propTypes = {
+    path: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    title: PropTypes.string,
+  };
+
+  static contextTypes = {
+    onSetTitle: PropTypes.func.isRequired,
+  };
+
+  render() {
+    this.context.onSetTitle(this.props.title);
+    return (
+      <div className={s.root}>
+        <div className={s.container}>
+          {this.props.path === '/' ? null : <h1>{this.props.title}</h1>}
+          <div dangerouslySetInnerHTML={{ __html: this.props.content || '' }} />
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default withStyles(ContentPage, s);
---
+++ src/components/MapBox/MapBox.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+// NotWorkingShimInstead import mapboxgl from 'mapbox-gl';
+require('../../../node_modules/mapbox-gl/js');
+
+class MapBox extends Component {
+    static propTypes = {
+        // Default map view
+        view: React.PropTypes.object,
+        // Style of map container
+        mapStyle: React.PropTypes.object,
+        // Current base layer
+        baselayer: React.PropTypes.string,
+        // Mapbox map token
+        token: React.PropTypes.string,
+        // onStyleEvent fired after style loaded.  Map object is passed
+        onStyleLoad: React.PropTypes.func
+    }
+
+    componentDidMount() {
+        mapboxgl.accessToken = this.props.token;
+        this.map = new mapboxgl.Map(this.props.view);
+
+        this.map.on('style.load', () => {
+            this.map.addSource('satellite', {
+                type: 'raster',
+                url: 'mapbox://mapbox.satellite'
+            });
+            this.map.addLayer({
+                'id': 'satellite',
+                'type': 'raster',
+                'source': 'satellite',
+                'layout': {
+                    'visibility': 'none'
+                }
+            });
+
+            if (this.props.onStyleLoad) {
+                this.props.onStyleLoad(this.map);
+            }
+        });
+    }
+
+    componentDidUpdate() {
+        if (this.props.baselayer === 'satellite') {
+            this.map.setLayoutProperty('satellite', 'visibility', 'visible');
+        } else {
+            this.map.setLayoutProperty('satellite', 'visibility', 'none');
+        }
+    }
+
+    componentWillUnmount() {
+        this.map.remove();
+    }
+
+    render() {
+        return (
+            <div>
+                <div style={this.props.mapStyle} id='map'></div>
+            </div>
+        );
+    }
+}
+
+export default MapBox;
